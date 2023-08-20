000b79b7d39cc9651a7d9321ce8abb32b94b3d91
Alina Arslanova <ararslanova@mail.ru>
2023-08-20 14:58:54 +0200

    add param checker

--- backup-script.sh
+++ backup-script.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Check if the right number of parameters are given
+if [ "$#" -ne 3 ]; then
+  echo "Usage: $0 <directory_to_backup> <compression_algorithm> <output_file_name>" >> error.log
+  exit 1
+fi
+
+# Assign the parameters to variables
 DIRECTORY_TO_BACKUP="$1"
 COMPRESSION_ALGORITHM="$2"
 OUTPUT_FILE_NAME="$3"
