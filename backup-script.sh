a9d70250b656ea9c615afa46f4d8fc0d16d542ae
Alina Arslanova <ararslanova@mail.ru>
2023-08-20 15:30:24 +0200

    add compression algorithm

--- backup-script.sh
+++ backup-script.sh
@@ -10,6 +10,7 @@ fi
 DIRECTORY_TO_BACKUP="$1"
 COMPRESSION_ALGORITHM="$2"
 OUTPUT_FILE_NAME="$3"
+PASSWORD="password"
 ERROR_LOG="error.log"
 
 echo "Starting the backup process..."
@@ -17,3 +18,22 @@ echo "Directory to backup: $DIRECTORY_TO_BACKUP"
 echo "Compression algorithm: $COMPRESSION_ALGORITHM"
 echo "Output file name: $OUTPUT_FILE_NAME"
 
+# Determine the compression flag for tar
+case $COMPRESSION_ALGORITHM in
+  "none")
+    tar_option=""
+    ;;
+  "gzip")
+    tar_option="z"
+    ;;
+  "bzip2")
+    tar_option="j"
+    ;;
+  "xz")
+    tar_option="J"
+    ;;
+  *)
+    echo "Unknown compression type. Use 'none', 'gzip', 'bzip2', or 'xz'." >> $ERROR_LOG
+    exit 1
+    ;;
+esac
